0efd3914d13dc4e5be95909762dd72e1d791fb94
Nagesh73308 <nageshmahesh111@gmail.com>
2024-04-20 11:44:08 +0530

    firstCommit

---
+++ cypress/e2e/googlemaps.cy.js
@@ -0,0 +1,13 @@
+describe('google maps',()=>{
+    it('google',()=>{
+        Cypress.on("uncaught:exception", () => {
+  
+            return false;
+          });
+        cy.visit('https://www.google.com/')
+        cy.get('textarea[name="q"]').type('google Maps{enter}')
+        cy.contains('Google Maps').click()
+        cy.get('form[jsaction="submit:omnibox.searchboxFormSubmit"]').type('gudibanda')
+        cy.get('button[id="searchbox-searchbutton"]').click()
+    })
+})
